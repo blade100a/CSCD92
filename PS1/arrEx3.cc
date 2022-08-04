35df2a370b3200bd8d6482911036623ddae00e69
Harri Pahirathan <harripahirathan@gmail.com>
2022-08-04 07:40:21 -0400

    Added counter and stdout

--- PS1/arrEx3.cc
+++ PS1/arrEx3.cc
@@ -13,12 +13,29 @@ using namespace std;
 int main() {
 	// Creates array with 100,000
 	int size = 100000;
-	int arr = new int[size];
+	int* arr = new int[size];
+	// Count array to hold the values counted
+	int count[19]= { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	// Fills array with equation given from exercise
 	for (int i = 0; i < size; i++){
 		arr[i] = floor(9*sin(i));
+		// Checks which number to count
+		for(int j = -9; j < 10; j++){
+			if(j == arr[i]){
+				// Adds to count
+				count[j+9]++;
+			}
+		}
 	}
+
+	// Prints values
+	for (int i = 0; i < 19; i++){
+		// Prints out values
+		std::cout << i-9 << " " << arr[i] << "\n";
+	}
+
 	// Deallocates the array
 	delete[] arr;
+	delete[] count;
 	return 0;
 }
