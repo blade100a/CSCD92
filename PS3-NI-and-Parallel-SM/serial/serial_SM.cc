4f0fc1c9646f0b397335c37d23f49f2e35c80746
Harri Pahirathan <harripahirathan@gmail.com>
2022-09-08 12:45:37 -0400

    Added comments

--- PS3-NI-and-Parallel-SM/serial/serial_SM.cc
+++ PS3-NI-and-Parallel-SM/serial/serial_SM.cc
@@ -14,15 +14,20 @@ PS3-NI-and-Parallel-SM Serial
 
 using namespace std;
 
+// Function to calulate the equation in the integral
 double eq(double x){
 	return (4/(1 + (pow(x, 2))));
 }
 
+// Function to calculate the Left Sum
 int left_sum(double N){
+	// Variables given to us
 	double H, xi, sum;
 	H = 1 / N; 
 	xi = 0;
+	// Holds the left sums
 	sum = 0;
+	// Loops through a total of the parition and adds the sum
 	for(int i = 0; i < N; i++){
 		xi = i*H;
 		sum += eq(xi);
@@ -31,12 +36,15 @@ int left_sum(double N){
 }
 
 int main(){
+	// The value of the parition
 	double N = 4000000;
+	// Will be the count mechanic
 	TickTock time;
 	std::cout << "Start of program\n";
 	std::cout << "Number of N: " << N << std::endl;
 	time.tick();
 	std::cout << "The Left Riemann Sum Value: " << left_sum(N) << std::endl;
+	// Stops the time
 	time.tock("Code took: ");
 	return 0; 
 }
