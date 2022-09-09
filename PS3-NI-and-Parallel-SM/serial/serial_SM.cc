950ae6e80e3690c5f65cac5b6d56c29f901b2a34
Harri Pahirathan <harripahirathan@gmail.com>
2022-09-09 10:14:36 -0400

    Added output file option

--- PS3-NI-and-Parallel-SM/serial/serial_SM.cc
+++ PS3-NI-and-Parallel-SM/serial/serial_SM.cc
@@ -1,4 +1,4 @@
-/*------------------ 
+/*------------------
 Harri Pahirathan
 CSCD92
 PS3-NI-and-Parallel-SM Serial
@@ -33,9 +33,12 @@ double left_sum(double N){
 int main(){
         // The value of the parition
         double N = 4000000;
+        // File for output of stdout
+        freopen("output_s.txt", "a", stdout);
         // Will be the count mechanic
         TickTock time;
-        std::cout << "Start of program\n";
+        std::cout << "Start of Serial program\n";
+        std::cout << "Number of Threads: 1\n";
         std::cout << "Number of N: " << N << std::endl;
         time.tick();
         double tot = left_sum(N);
@@ -45,6 +48,7 @@ int main(){
         // Numerical error
         double pi = 2 * acos(0.0);
         std::cout << "Numerical Error(Difference pi and sum): " << pi - tot << std::endl;
+        std::cout << "\n";
         return 0;
 }
 
